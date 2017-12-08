37ba57f1d3b1e2c5918693b32d23a9cd2f15b091
王庆学 <wangqingxue@wangqingxuedeMac.local>
2017-12-09 00:55:24 +0800

    Initial Commit

---
+++ QXWPagedFlowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QXWPagedFlowDemo
+//
+//  Created by 王庆学 on 2017/12/9.
+//  Copyright © 2017年 王庆学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QXWPagedFlowDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QXWPagedFlowDemo
+//
+//  Created by 王庆学 on 2017/12/9.
+//  Copyright © 2017年 王庆学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
